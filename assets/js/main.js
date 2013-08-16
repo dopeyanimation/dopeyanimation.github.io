6e76a780ac8a221d6d531f0753e77ebae7bd952f
davissorenson <davis@davissorenson.com>
2013-08-16 23:36:18 +0300

    Start of Emberification

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
+DP = Em.Application.create();
 
+DP.ApplicationController = Ember.Controller.extend({
+	currentPathChanged: function() {
+		var page;
+
+		Ember.run.next(function() {
+			if (!Ember.isNone(ga)) {
+				page = window.location.hash.length > 0 ?
+					   window.location.hash.substring(1) :
+					   window.location.pathname;
+
+				ga('send', 'pageview');
+			}
+		});
+	}.observes('currentPath')
+});
+
+DP.Router.map(function() {
+	this.route('download', { path: '/download'});
+	this.route('features', { path: '/features'});
+	this.route('about', { path: '/about'});
+	this.route('help', { path: '/help'});
+});
